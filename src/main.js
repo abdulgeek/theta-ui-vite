9104d600820940dbd6cf66993f3f21186c1471d8
Abdul <abdul.rahman@geekseat.com.au>
2021-09-22 11:34:39 +0700

    add vuex

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
-const app = createApp(App).use(router)
+const app = createApp(App).use(store).use(router)
 app.use(vuetify)
 
 app.mount('#app')
